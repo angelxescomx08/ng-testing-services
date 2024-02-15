f117efc85c820c50e74900bac2f9af6b9a27ad7f
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-15 12:24:43 -0600

    products service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './components/products/products.component';
+import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { ProductsService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ProductsComponent,
+    PicoPreviewComponent,
+  ],
+  providers: [ProductsService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public products: Product[] = [];
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsService.getAllSimple().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
