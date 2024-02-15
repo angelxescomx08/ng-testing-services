0be2ded3d51a6dfe2a2b0ba335531e36289ca910
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-15 11:01:59 -0600

    maquetacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { ProductsComponent } from './components/products/products.component';
+import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'pico-preview',
+    component: PicoPreviewComponent,
+  },
+];
