af2009a98769d199dc6ea8f0ddaaa41eded8eec5
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-15 18:11:47 -0600

    getAll pruebas

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -25,12 +25,44 @@ describe('ProductsService', () => {
   });
 
   describe('getAllSimple', () => {
+    const productsMock: Product[] = generateManyProducts(2);
     it('should have two products', (doneFn) => {
       productsService.getAllSimple().subscribe((products) => {
-        expect(products.length).toBe(2);
+        expect(products.length).toBe(productsMock.length);
         doneFn();
       });
+      const url = `${environment.API_URL}/api/v1/products`;
+      const testRequest = controller.expectOne(url);
+      testRequest.flush(productsMock);
+      controller.verify();
+    });
+  });
+
+  describe('getAll', () => {
+    it('should have two products', (doneFn) => {
       const productsMock: Product[] = generateManyProducts(2);
+      productsService.getAll().subscribe((products) => {
+        expect(products.length).toBe(productsMock.length);
+        doneFn();
+      });
+      const url = `${environment.API_URL}/api/v1/products`;
+      const testRequest = controller.expectOne(url);
+      testRequest.flush(productsMock);
+      controller.verify();
+    });
+
+    it('should return taxes', (doneFn) => {
+      const productsMock: Product[] = generateManyProducts(2).map(
+        (product, index) => ({
+          ...product,
+          price: index % 2 === 0 ? 100 : 200,
+        })
+      );
+      productsService.getAll().subscribe((products) => {
+        expect(products[0].taxes).toBe(19);
+        expect(products[1].taxes).toBe(38);
+        doneFn();
+      });
       const url = `${environment.API_URL}/api/v1/products`;
       const testRequest = controller.expectOne(url);
       testRequest.flush(productsMock);
