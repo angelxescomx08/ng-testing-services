1757ddb045bc0d3dd349aab48d9dae2f23b98579
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-20 11:59:30 -0600

    fakeAsync y tick

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,11 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
 import { ProductComponent } from '../product/product.component';
 import { ProductsService } from '../../services/product.service';
 import { generateManyProducts } from '../../models/product.mock';
-import { of } from 'rxjs';
+import { defer, of } from 'rxjs';
 import { By } from '@angular/platform-browser';
+import { Product } from '../../models/product.model';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -55,5 +61,44 @@ describe('ProductsComponent', () => {
         expectedImg.nativeElement.src
       );
     });
+
+    it('should change status "loading" => "success"', fakeAsync(() => {
+      // Arrange
+      const productsMock = generateManyProducts(10);
+      productsService.getAll.and.returnValue(
+        defer(() => Promise.resolve<Product[]>(productsMock))
+      );
+      // Act
+      component.getAllProducts();
+      fixture.detectChanges();
+      // Assert
+      expect(component.status).toBe('loading');
+      //tick sirve para ejecutar todo lo que esta pendiente observables,
+      //setTimeout, promesas entre otras cosas, debe ser siempre usada con
+      //fakeAsync
+      tick();
+      fixture.detectChanges();
+      expect(component.status).toBe('success');
+    }));
+
+    it('should change status "loading" => "error"', fakeAsync(() => {
+      // Arrange
+      const productsMock = generateManyProducts(10);
+      productsService.getAll.and.returnValue(
+        defer(() => Promise.reject<Product[]>(productsMock))
+      );
+      // Act
+      component.getAllProducts();
+      fixture.detectChanges();
+      // Assert
+      expect(component.status).toBe('loading');
+      //Se le puede pasar un número para que espere ese tiempo, esto es
+      //porque los setTimeout o cosas con un tiempo indefinido podrían no
+      //ejecutarse a tiempo, dale un número más grande del que tienes en
+      //setTimeout
+      tick(2000);
+      fixture.detectChanges();
+      expect(component.status).toBe('error');
+    }));
   });
 });
